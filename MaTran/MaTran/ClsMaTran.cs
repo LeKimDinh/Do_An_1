791969eff5499e7bad4952c9a0f12eeeea4e1dad
MrXiX <43243006+MrXiX@users.noreply.github.com>
2019-09-16 08:43:14 +0700

    Up lần 1

---
+++ MaTran/MaTran/ClsMaTran.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaTran
+{
+    public class ClsMaTran
+    {
+        private int soDong;
+        private int soCot;
+        private int[][] matrix;
+        public ClsMaTran()
+        {
+            this.soDong = 0;
+            this.soCot = 0;
+        }
+        public int SoDong { get => soDong; set => soDong = value; }
+        public int SoCot { get => soCot; set => soCot = value; }
+        public int[][] Matrix { get => matrix; set => matrix = value; }
+
+        public ClsMaTran Cong(ClsMaTran A, ClsMaTran B)
+        {
+            ClsMaTran C = new ClsMaTran();
+            
+            if(A.soDong != B.soDong || A.soCot != B.soCot)
+            {
+                return null;
+            }
+            for (int i = 0; i < A.soDong; i++)
+            {
+                for (int j = 0; j < A.soCot; j++)
+                {
+                    C.matrix[i][j] = A.matrix[i][j] + B.matrix[i][j];
+                }
+            }
+            return C;
+        }
+        public ClsMaTran Tru(ClsMaTran A, ClsMaTran B)
+        {
+            ClsMaTran C = new ClsMaTran();
+
+            if (A.soDong != B.soDong || A.soCot != B.soCot)
+            {
+                return null;
+            }
+            for (int i = 0; i < A.soDong; i++)
+            {
+                for (int j = 0; j < A.soCot; j++)
+                {
+                    C.matrix[i][j] = A.matrix[i][j] - B.matrix[i][j];
+                }
+            }
+            return C;
+        }
+        public ClsMaTran Nhan(ClsMaTran A, ClsMaTran B)
+        {
+            ClsMaTran C = new ClsMaTran();
+
+        }
+    }
+}
